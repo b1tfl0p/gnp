10e7384be2e64179573892ca32c07a61712dad60
b1tfl0p <b1tfl0p@proton.me>
2025-08-14 13:52:11 -0700

    Implement unix datagram socket server

--- ch07/echo/echo.go
+++ ch07/echo/echo.go
@@ -3,6 +3,7 @@ package echo
 import (
 	"context"
 	"net"
+	"os"
 )
 
 func streamingEchoServer(ctx context.Context, network string, addr string) (net.Addr, error) {
@@ -43,4 +44,36 @@ func streamingEchoServer(ctx context.Context, network string, addr string) (net.
 	}()
 
 	return s.Addr(), nil
+}
+
+func datagramEchoServer(ctx context.Context, network string, addr string) (net.Addr, error) {
+	s, err := net.ListenPacket(network, addr)
+	if err != nil {
+		return nil, err
+	}
+
+	go func() {
+		go func() {
+			<-ctx.Done()
+			_ = s.Close()
+			if network == "unixgram" {
+				_ = os.Remove(addr)
+			}
+		}()
+
+		buf := make([]byte, 1024)
+		for {
+			n, clientAddr, err := s.ReadFrom(buf)
+			if err != nil {
+				return
+			}
+
+			_, err = s.WriteTo(buf[:n], clientAddr)
+			if err != nil {
+				return
+			}
+		}
+	}()
+
+	return s.LocalAddr(), nil
 }
